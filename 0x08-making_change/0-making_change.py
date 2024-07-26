437bf088f533d912459d95dddfbeb4c7ce2da741
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-26 04:19:42 +0300

    Pycodesytle fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Make change challenge"""
+
+
 def makeChange(coins, total):
     """Given a pile of coins of different values,
     determine the fewest number of coins needed
@@ -7,16 +9,19 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
-    # Initialize a list to store the minimum number of coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: no coins are needed to make the amount 0
 
-    # Compute the minimum number of coins needed for each amount from 1 to total
+    # Initialize a list to store the minimum number of coins
+    # needed for each amount
+    dp = [float('inf')] * (total + 1)
+    # Base case: no coins are needed to make the amount 0
+    dp[0] = 0
+    # Compute the minimum number of coins needed for
+    # each amount from 1 to total
     for i in range(1, total + 1):
         for coin in coins:
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If dp[total] is still infinity, it means that total cannot be made with the given coins
+    # If dp[total] is still infinity, it means that total cannot
+    # be made with the given coins
     return dp[total] if dp[total] != float('inf') else -1
