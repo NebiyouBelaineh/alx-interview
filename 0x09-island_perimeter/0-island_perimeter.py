db90398ad92f9e9576872a3a1f956249afd2513c
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-04 15:56:26 +0300

    Add solution for island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Island Perimeter Challenge
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    if len(grid) == 0:
+        return
+
+    countPerimeter = 0
+    # loop through each array
+    # loop through the array within
+    # check for land(i.e values with 1) a the top, bottom, left
+    # and right of each point where there is land
+    # if next value in any direction is 0, increment countPerimeter,
+    # else move to next
+    for m, i in enumerate(grid):
+        for n, j in enumerate(i):
+            if j == 0:
+                continue
+            # check top and bottom sides
+            if grid[m - 1][n] == 0:
+                countPerimeter += 1
+            if grid[m + 1][n] == 0:
+                countPerimeter += 1
+            # check left and right sides
+            if grid[m][n - 1] == 0:
+                countPerimeter += 1
+            if grid[m][n + 1] == 0:
+                countPerimeter += 1
+    return countPerimeter
