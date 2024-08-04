e1c5fc399c69df2bce8d63503856b44373f3017a
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-04 16:25:07 +0300

    Account for grid dimension constraints and check if grid is not none

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,7 +6,9 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-    if len(grid) == 0:
+    if not grid or len(grid) == 0:
+        return 0
+    if len(grid) > 100 or len(grid[0]) > 100:
         return 0
 
     countPerimeter = 0
@@ -16,18 +18,19 @@ def island_perimeter(grid):
     # and right of each point where there is land
     # if next value in any direction is 0, increment countPerimeter,
     # else move to next
-    for m, i in enumerate(grid):
-        for n, j in enumerate(i):
-            if j == 0:
-                continue
-            # check top and bottom sides
-            if grid[m - 1][n] == 0:
-                countPerimeter += 1
-            if grid[m + 1][n] == 0:
-                countPerimeter += 1
-            # check left and right sides
-            if grid[m][n - 1] == 0:
-                countPerimeter += 1
-            if grid[m][n + 1] == 0:
-                countPerimeter += 1
+    rows = len(grid)
+    cols = len(grid[0])
+    for m in range(rows):
+        for n in range(cols):
+            if grid[m][n] == 1:
+                # check top and bottom sides
+                if m == 0 or grid[m - 1][n] == 0:
+                    countPerimeter += 1
+                if m == rows - 1 or grid[m + 1][n] == 0:
+                    countPerimeter += 1
+                # check left and right sides
+                if n == 0 or grid[m][n - 1] == 0:
+                    countPerimeter += 1
+                if n == cols - 1 or grid[m][n + 1] == 0:
+                    countPerimeter += 1
     return countPerimeter
